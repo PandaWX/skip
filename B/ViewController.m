cfd763071384007d11f51d76e076f2fcc9c7fb0b
“pandaWX” <328962094@qq.com>
2016-05-03 15:50:37 +0800

    no message

--- B/ViewController.m
+++ B/ViewController.m
@@ -18,6 +18,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)tipToA:(id)sender {
+    
+    NSLog(@"to A");
+    //
+    NSString *paramStr = [NSString stringWithFormat:@"myAppTest://username=%@&age=%@&address=%@", @"test123", @"100", @"上海市"];
+    NSURL *url = [NSURL URLWithString:[paramStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    [[UIApplication sharedApplication] openURL:url];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
